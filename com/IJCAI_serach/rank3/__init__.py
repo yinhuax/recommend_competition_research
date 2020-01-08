918183f745b8f1d079099e774251cc9003829b86
yinhuaxi <597290963@qq.com>
2020-01-08 08:41:17 +0800

    feat(开始研究rank9):
    NO
    
    BREAKING CHANGE: NO
    
    Closes NO

---
+++ com/IJCAI_serach/rank3/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : Mike
+# @Contact : 597290963@qq.com
+# @Time    : 2020/1/7 21:30
+# @File    : __init__.py.py
+
---
+++ com/IJCAI_serach/rank9/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : Mike
+# @Contact : 597290963@qq.com
+# @Time    : 2020/1/7 23:17
+# @File    : __init__.py.py
+
